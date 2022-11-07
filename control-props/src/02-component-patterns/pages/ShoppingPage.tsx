4f6020e554537bd3d999e082ac342bbcec6a580b
Maxi Diaz <maxi.diaz66@gmail.com>
2022-11-07 15:46:30 -0300

    chore(control-props): add cart state and onChange funtionality

--- control-props/src/02-component-patterns/pages/ShoppingPage.tsx
+++ control-props/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import {
   ProductButtons,
   ProductCard,
   ProductImage,
   ProductTittle,
 } from "../components";
+import { Product } from "../interfaces/interfaces";
 import "../styles/custom-styles.css";
 
 const product = {
@@ -12,7 +14,27 @@ const product = {
   img: "./coffee-mug.png",
 };
 
+const product2 = {
+  id: "2",
+  title: "CoffeMug - Meme",
+  img: "./coffee-mug2.png",
+};
+
+const products: Product[] = [product, product2];
+
+interface ProductInCart extends Product {
+  count: number;
+}
+
 export const ShoppingPage = () => {
+  const [shoppingCart, setShoppingCart] = useState<{
+    [key: string]: ProductInCart;
+  }>({});
+
+  const onProductCountChange = () => {
+    console.log("onProductCountChange");
+  };
+
   return (
     <div>
       <h1>Shopping Store</h1>
@@ -24,36 +46,28 @@ export const ShoppingPage = () => {
           flexWrap: "wrap",
         }}
       >
-        <ProductCard product={product} className="bg-dark text-white">
+        {products.map((p) => (
+          <ProductCard
+            product={p}
+            className="bg-dark text-white"
+            key={p.id}
+            onChange={() => onProductCountChange()}
+          >
+            <ProductImage className="custom-image"></ProductImage>
+            <ProductTittle className="text-tittle"></ProductTittle>
+            <ProductButtons className="custom-buttons"></ProductButtons>
+          </ProductCard>
+        ))}
+      </div>
+
+      <div className="shopping-cart">
+        <ProductCard
+          product={product2}
+          className="bg-dark text-white"
+          style={{ width: "100px" }}
+        >
           <ProductImage className="custom-image"></ProductImage>
-          <ProductTittle className="text-tittle"></ProductTittle>
           <ProductButtons className="custom-buttons"></ProductButtons>
-        </ProductCard>
-
-        {/* Puede realizarse de ambas maneras */}
-
-        <ProductCard product={product}>
-          <ProductCard.Image className="custom-image"></ProductCard.Image>
-          <ProductCard.Tittle
-            tittle="Hola mundo"
-            className="text-tittle"
-          ></ProductCard.Tittle>
-          <ProductCard.Buttons></ProductCard.Buttons>
-        </ProductCard>
-
-        <ProductCard product={product} style={{ backgroundColor: "#70D1F8" }}>
-          <ProductImage></ProductImage>
-          <ProductTittle
-            style={{
-              fontWeight: "bold",
-              color: "black",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          ></ProductTittle>
-          <ProductButtons
-            style={{ display: "flex", justifyContent: "center" }}
-          ></ProductButtons>
         </ProductCard>
       </div>
     </div>
