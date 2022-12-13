2cf5aed1183c8d56edb8cab667b74c21bb6fd53a
Maxi Diaz <maxi.diaz66@gmail.com>
2022-12-13 12:30:27 -0300

    chore(md-product-card): make test and changes in package.json

--- md-product-card/test/components/ProductCard.test.tsx
+++ md-product-card/test/components/ProductCard.test.tsx
@@ -1,61 +1,43 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
+import React from 'react';
+import renderer from 'react-test-renderer';
 import { ProductCard } from '../../src/components';
 import { product1 } from '../data/products';
 
 const { act } = renderer;
 
 describe('ProductCard', () => {
-    
-    test('debe de mostrar el componente correctamente', () => {
+  test('debe de mostrar el componente correctamente', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product1}>
+        {() => <h1>Product Card</h1>}
+      </ProductCard>
+    );
 
-        const wrapper = renderer.create(
-            <ProductCard product={ product1 }>
-                {
-                    () => (
-                        <h1>Product Card</h1>
-                    )
-                }
-            </ProductCard>
-        )
-        
-        expect( wrapper.toJSON() ).toMatchSnapshot();
-        
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  test('debe de incrementar el contador', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product1}>
+        {({ count, increaseBy }) => (
+          <>
+            <h1>Product Card</h1>
+            <span>{count}</span>
+            <button onClick={() => increaseBy(1)}></button>
+          </>
+        )}
+      </ProductCard>
+    );
+
+    let tree = wrapper.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    act(() => {
+      (tree as any).children[2].props.onClick();
     });
 
-    test('debe de incrementar el contador', () => {
+    tree = wrapper.toJSON();
 
-
-        const wrapper = renderer.create(
-            <ProductCard product={ product1 }>
-                {
-                    ({ count, increaseBy }) => (
-                        <>
-                            <h1>Product Card</h1>
-                            <span>{ count }</span>
-                            <button onClick={ () => increaseBy(1)}></button>
-                        </>
-                    )
-                }
-            </ProductCard>
-        );
-
-        let tree = wrapper.toJSON();
-        expect( tree ).toMatchSnapshot();
-
-        act(() => {
-            (tree as any).children[2].props.onClick();
-        })
-
-        tree = wrapper.toJSON();
-
-        expect((tree as any).children[1].children[0]).toBe('1')
-
-
-
-    })
-    
-    
-
-})
-
+    expect((tree as any).children[1].children[0]).toBe('1');
+  });
+});
