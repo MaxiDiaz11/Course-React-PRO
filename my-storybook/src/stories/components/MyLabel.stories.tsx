96a806fedc6082bc55ba2e8f08f9915b99bcb5c8
Maxi Diaz <maxi.diaz66@gmail.com>
2022-12-21 16:13:17 -0300

    chore(my-storybook): add story with Label component

---
+++ my-storybook/src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,11 @@
+import { MyLabel } from "../../components/MyLabel";
+
+export default {
+  title: "UI/Etiquetas/MyLabel",
+  component: MyLabel,
+};
+
+const Template = () => <MyLabel></MyLabel>;
+
+export const BasicLabel = Template.bind({});
+export const SecondaryLabel = Template.bind({});
