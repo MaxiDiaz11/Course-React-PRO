041972d2d824532d59a1111d601b2bdb56e49a44
Maxi Diaz <maxi.diaz66@gmail.com>
2022-12-22 15:26:06 -0300

    chore(my-storybook): add last properties and build folder - end of project

--- my-storybook/src/components/MyLabel.tsx
+++ my-storybook/src/components/MyLabel.tsx
@@ -9,11 +9,33 @@ export interface MyLabelProps {
    * What size to use
    */
   size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * What capitalize to use
+   */
+  allCaps?: boolean;
+  /**
+   * What color to use
+   */
+  color?: "primary" | "secondary" | "terciary";
+  /**
+   * What background color to use
+   */
+  fontColor?: string;
 }
 
 export const MyLabel = ({
+  allCaps = false,
+  color = "primary",
+  fontColor,
   label = "No Label",
   size = "normal",
 }: MyLabelProps) => {
-  return <span className={`${size}`}>{label}</span>;
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
--- my-storybook/src/stories/components/MyLabel.stories.tsx
+++ my-storybook/src/stories/components/MyLabel.stories.tsx
@@ -4,6 +4,10 @@ import { MyLabel } from "../../components/MyLabel";
 export default {
   title: "UI/Etiquetas/MyLabel",
   component: MyLabel,
+  argTypes: {
+    color: { control: "select" },
+    fontColor: { control: "color" },
+  },
 } as ComponentMeta<typeof MyLabel>;
 
 const Template: ComponentStory<typeof MyLabel> = (args) => (
@@ -12,6 +16,30 @@ const Template: ComponentStory<typeof MyLabel> = (args) => (
 
 export const BasicLabel = Template.bind({});
 BasicLabel.args = {
-  label: "Hola Mundo",
+  allCaps: false,
   size: "normal",
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  size: "normal",
+  allCaps: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: "normal",
+  color: "secondary",
+};
+
+export const Terciary = Template.bind({});
+Terciary.args = {
+  size: "normal",
+  color: "terciary",
+};
+
+export const FontColor = Template.bind({});
+FontColor.args = {
+  size: "h1",
+  fontColor: "#5517ac",
 };
