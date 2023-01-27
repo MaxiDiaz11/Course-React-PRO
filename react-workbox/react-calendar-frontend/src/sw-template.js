726159eeb90fec806ae639a814a30ae3ae190800
Maxi Diaz <maxi.diaz66@gmail.com>
2023-01-27 12:24:18 -0300

    chore(react-workbox): refactor code and end of project

--- react-workbox/react-calendar-frontend/src/sw-template.js
+++ react-workbox/react-calendar-frontend/src/sw-template.js
@@ -2,31 +2,50 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
+workbox.loadModule("workbox-background-sync");
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-registerRoute(
-  new RegExp("http://localhost:4000/api/auth/renew"),
-  new NetworkFirst()
-);
+const cacheNetworkOnly = ["/api/auth/renew", "/api/events"];
+
+const cacheNetworkFirst = [
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css",
+];
+
+registerRoute(({ request, url }) => {
+  if (cacheNetworkOnly.includes(url.pathname)) return true;
+  return false;
+}, new NetworkFirst());
+
+registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.href)) return true;
+  return false;
+}, new CacheFirst());
+
+//Posteos offline
+const bgSyncPlugin = new BackgroundSyncPlugin("posteos-offline", {
+  maxRetentionTime: 24 * 60,
+});
 
 registerRoute(
   new RegExp("http://localhost:4000/api/events"),
-  new NetworkFirst()
+  new NetworkOnly({ plugins: [bgSyncPlugin] }),
+  "POST"
 );
 
 registerRoute(
-  new RegExp(
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-  ),
-  new CacheFirst()
+  new RegExp("http://localhost:4000/api/events/"),
+  new NetworkOnly({ plugins: [bgSyncPlugin] }),
+  "DELETE"
 );
 
 registerRoute(
-  new RegExp(
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
-  ),
-  new CacheFirst()
+  new RegExp("http://localhost:4000/api/events/"),
+  new NetworkOnly({ plugins: [bgSyncPlugin] }),
+  "PUT"
 );
